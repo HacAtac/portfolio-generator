96a16fa2ef3489a566b6f0eda30b8f477445fab2
HacAtac <jhack00@icloud.com>
2021-09-29 00:10:55 -0500

    added when method to put previous answers in array

--- app.js
+++ app.js
@@ -37,11 +37,28 @@ const promptUser = () => {
         }
       }
     },
+
+    {
+      type: 'confirm',
+      name: 'confirmAbout',
+      message: 'Would you like to enter some inf about yourself for an "About" section?',
+      default: true
+    },
+
     {
       type: 'input',
       name: 'about',
-      message: 'Provide some information about yourself:'
+      message: 'Provide some information about yourself:',
+      when: ({ confirmAbout }) => {
+        if (confirmAbout) {
+          return true;
+        } else {
+          return false;
+        }
+      }
     }
+
+  
   ]);
 };
 
